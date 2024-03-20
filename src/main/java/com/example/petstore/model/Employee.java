080903edb816df2b03cbede627a7ed57a8628ad4
VadimHerasimovich <vadimpoi2005@mail.ru>
2024-03-20 03:43:07 +0300

    v2.1.1

--- src/main/java/com/example/petstore/model/Employee.java
+++ src/main/java/com/example/petstore/model/Employee.java
@@ -3,16 +3,24 @@ package com.example.petstore.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Data
 @Table(name = "employees")
 public class Employee {
     @Id
     @GeneratedValue
-    private Long eid;
+    private Long id;
     private String firstName;
     @Column(unique = true)
     private String phone;
     private String password;
     private String role;
+    @ManyToMany
+    @JoinTable(name = "employee_pets",
+            joinColumns = @JoinColumn(name = "employee_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "pet_id", referencedColumnName = "id") )
+    private Set<Pet> pets = new HashSet<>();
 }
--- src/main/java/com/example/petstore/model/Pet.java
+++ src/main/java/com/example/petstore/model/Pet.java
@@ -3,6 +3,9 @@ package com.example.petstore.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @Entity
 @Table(name = "pets")
@@ -16,4 +19,9 @@ public class Pet {
     private String status;
     @Column(unique = true)
     private String phone;
+    @ManyToMany
+    @JoinTable(name = "employee_pets",
+            joinColumns = @JoinColumn(name = "pet_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "employee_id", referencedColumnName = "id") )
+    private Set<Employee> employees = new HashSet<>();
 }
