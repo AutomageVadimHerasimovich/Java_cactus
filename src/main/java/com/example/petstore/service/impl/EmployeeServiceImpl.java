af208beaf3279ee0f736047f23fb58c3b62d3e60
VadimHerasimovich <vadimpoi2005@mail.ru>
2024-04-04 01:19:31 +0300

    v4.0.1

--- src/main/java/com/example/petstore/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/petstore/service/impl/EmployeeServiceImpl.java
@@ -4,60 +4,72 @@ import com.example.petstore.model.Employee;
 import com.example.petstore.repository.EmployeeRepository;
 import com.example.petstore.service.EmployeeService;
 import com.example.petstore.service.cache.EmployeeCache;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
+/**
+ * The service class for the Employee entity.
+ */
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final EmployeeRepository employeeRepository;
-    private final EmployeeCache employeeCache;
-    @Autowired
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository, EmployeeCache employeeCache) {
-        this.employeeRepository = employeeRepository;
-        this.employeeCache = employeeCache;
-    }
-    public List<Employee> getEmployees() {
-        return employeeRepository.findAll();
-    }
-    //@Cacheable(value = "employees")
-    public Employee saveEmployee(Employee employee) {
-        //return
-        employeeRepository.save(employee);
-        employeeCache.put(employee.getPhone(), employee);
-        return employee;
-    }
-    //@Cacheable(value = "employees", key = "#phone")
-    public Employee getEmployeeByPhone(String phone) {
-        //return employeeRepository.findEmployeeByPhone(phone);
-        return employeeCache.get(phone);
-    }
-    //@CachePut(value = "employees", key = "#employee.phone")
-    public Employee updateEmployee(Employee employee) {
-        Employee existingEmployee = employeeRepository.findEmployeeByPhone(employee.getPhone());
-        if (existingEmployee != null) {
-            existingEmployee.setFirstName(employee.getFirstName());
-            existingEmployee.setRole(employee.getRole());
-            existingEmployee.setPassword(employee.getPassword());
-            employeeCache.put(existingEmployee.getPhone(), existingEmployee);
-        }
-        assert existingEmployee != null;
-        return employeeRepository.save(existingEmployee);
-    }
 
-    public List<Employee> findEmployeesByFirstNameAndRole(String name, String role) {
-        return employeeRepository.findEmployeesByFirstNameAndRole(name, role);
-    }
+  private final EmployeeRepository employeeRepository;
 
-    @Transactional
-    //@CacheEvict(value = "employees", key = "#phone")
-    public void deleteEmployee(String phone) {
-        employeeRepository.deleteEmployeeByPhone(phone);
-        employeeCache.evict(phone);
+  private final EmployeeCache employeeCache;
+
+  @Autowired
+  public EmployeeServiceImpl(EmployeeRepository employeeRepository, EmployeeCache employeeCache) {
+    this.employeeRepository = employeeRepository;
+    this.employeeCache = employeeCache;
+  }
+
+  public List<Employee> getEmployees() {
+    return employeeRepository.findAll();
+  }
+
+  /**
+   * Saves the given employee.
+   *
+   * @param employee the employee to save
+   * @return the saved employee
+   */
+  public Employee saveEmployee(Employee employee) {
+    employeeRepository.save(employee);
+    employeeCache.put(employee.getPhone(), employee);
+    return employee;
+  }
+
+  public Employee getEmployeeByPhone(String phone) {
+    return employeeCache.get(phone);
+  }
+
+  /**
+   * Updates the given employee.
+   *
+   * @param employee the employee to update
+   * @return the updated employee
+   */
+  public Employee updateEmployee(Employee employee) {
+    Employee existingEmployee = employeeRepository.findEmployeeByPhone(employee.getPhone());
+    if (existingEmployee != null) {
+      existingEmployee.setFirstName(employee.getFirstName());
+      existingEmployee.setRole(employee.getRole());
+      existingEmployee.setPassword(employee.getPassword());
+      employeeCache.put(existingEmployee.getPhone(), existingEmployee);
     }
+    assert existingEmployee != null;
+    return employeeRepository.save(existingEmployee);
+  }
+
+  public List<Employee> findEmployeesByFirstNameAndRole(String name, String role) {
+    return employeeRepository.findEmployeesByFirstNameAndRole(name, role);
+  }
+
+  @Transactional
+  public void deleteEmployee(String phone) {
+    employeeRepository.deleteEmployeeByPhone(phone);
+    employeeCache.evict(phone);
+  }
 }
