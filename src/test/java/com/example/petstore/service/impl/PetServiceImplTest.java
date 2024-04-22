f8d9fd847fd15cd5c0a5327660b504ce8231e3ee
VadimHerasimovich <vadimpoi2005@mail.ru>
2024-04-22 23:46:18 +0300

    v5.2

--- src/test/java/com/example/petstore/service/impl/PetServiceImplTest.java
+++ src/test/java/com/example/petstore/service/impl/PetServiceImplTest.java
@@ -9,8 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
 import java.util.Arrays;
 import java.util.List;
+
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
